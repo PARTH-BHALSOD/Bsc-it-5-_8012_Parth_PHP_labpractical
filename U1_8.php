b3b2bebe23af633d7224b2bb464a3a9597ec7a21
Parth Bhalsod <parthbhalsod2@gmail.com>
2026-06-04 10:57:25 +0530

    Update U1_8.php

--- U1_8.php
+++ U1_8.php
@@ -14,6 +14,28 @@
     echo $p."<br>";
     }
 
+    $arr = array(
+    "name" => "Parth",
+    "city" => "Rajkot",
+    "age"  => 20
+    );
+
+    foreach($arr as $key => $value){
+        echo $key . " : " . $value . "<br>";
+    }
+
+    $arr = array(
+    array("Parth", 20, "Rajkot"),
+    array("Rahul", 22, "Ahmedabad"),
+    array("Amit", 21, "Surat")
+    );
+
+    foreach($arr as $row){
+        foreach($row as $value){
+            echo $value . " ";
+        }
+        echo "<br>";
+    }
 
     ?>
 </body>
